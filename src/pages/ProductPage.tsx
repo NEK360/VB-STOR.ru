c0fd269564aba9bab9afaed5c318dad2b87d035f
NEK360 <nikitabaranov1789@gmail.com>
2026-07-29 03:55:16 +0300

    Update ProductPage.tsx

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -302,7 +302,7 @@ export default function ProductPage() {
                 </div>
               )}
 
-             
+            
 
               <div className="absolute top-4 left-4 flex flex-col gap-2">
                 {product.isNew && (
