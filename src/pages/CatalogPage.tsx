ecbe7ad80b0acd7730636c5de7ff888c6902b1f9
NEK360 <nikitabaranov1789@gmail.com>
2026-07-30 04:46:12 +0300

    Update CatalogPage.tsx

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -370,7 +370,7 @@ export default function CatalogPage() {
             <p className="text-white/20 text-sm mt-2">Попробуйте изменить фильтры</p>
           </motion.div>
         ) : (
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+         <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6 mt-8">
             {filtered.map((product, i) => (
               <ProductCard key={product.id} product={product} index={i} />
             ))}
