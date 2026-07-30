9bfac00a18c145a7b601f7510e460ceb62cfcd0e
NEK360 <nikitabaranov1789@gmail.com>
2026-07-30 17:43:18 +0300

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -345,16 +345,16 @@ export async function preloadProduct(id: string): Promise<void> {
 
 export async function getProductById(id: string): Promise<Product | undefined> {
   const products = await loadProducts();
-console.log({
-  gender: p.gender,
-  article: p.article,
-});
+
   return products.find((p) => String(p.id) === String(id));
 }
 
 export async function getNewProducts(): Promise<Product[]> {
   const products = await loadProducts();
-
+console.log({
+  gender: p.gender,
+  article: p.article,
+});
   return products.filter((p) => p.isNew);
 }
 
