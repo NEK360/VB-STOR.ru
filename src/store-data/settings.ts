5377b83d6b0b5558780b4d1e535012341158d883
NEK360 <nikitabaranov1789@gmail.com>
2026-08-02 03:43:56 +0300

    Update settings.ts

--- src/store-data/settings.ts
+++ src/store-data/settings.ts
@@ -1,6 +1,6 @@
 export const settings = {
   storeName: "VB STORE",
-  storeSlogan: "Premium Fashion & Accessories",
+  storeSlogan: "Выгодный магазин обуви и аксессуаров",
   currency: "₽",
   currencyCode: "RUB",
   phone: "+7 918 797-02-30",
